41d8e27ce4f1410e4aa259d51c15cb6747b042e1
NSproGAMERSYT <87557923+RHYTHMHACKER@users.noreply.github.com>
2022-04-28 18:12:34 +0530

    Update Mspam.sh

--- Mspam.sh
+++ Mspam.sh
@@ -29,13 +29,13 @@ banner() {
     if ! [ -x "$(command -v figlet)" ]; then
         echo 'Introducing Mspam'
     else
-        figlet NSproYT
+        figlet Mspam
     fi
     if ! [ -x "$(command -v toilet)" ]; then
         echo -e "\e[4;34m This Spamer Was Created By \e[1;32mNSproYT \e[0m"
     else
         echo -e "\e[1;34mCreated By \e[1;34m"
-        toilet -f mono12 -F border Mspam
+        toilet -f mono12 -F border NSproYT
     fi
     echo -e "\e[1;34m For Any Queries Join Me!!!\e[0m"
     echo -e "\e[1;32m           Telegram: https://t.me/RHYTHMHACKER\e[0m"
